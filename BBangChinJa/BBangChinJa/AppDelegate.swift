e6a4f7586d93d012dcc28c99a58d58f02ce20baf
동영 <arhaxoda@naver.com>
2024-05-13 17:20:45 +0900

    아이디 숨기기

--- BBangChinJa/BBangChinJa/AppDelegate.swift
+++ BBangChinJa/BBangChinJa/AppDelegate.swift
@@ -12,10 +12,11 @@ import NMapsMap
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
+	let id = Bundle.main.idKey
 
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
 		FirebaseApp.configure()
-		NMFAuthManager.shared().clientId = "ybkzrvowft"
+		NMFAuthManager.shared().clientId = id
 		return true
 	}
 
---
+++ BBangChinJa/BBangChinJa/Extensions/BBangChinJa++Bundle.swift
@@ -0,0 +1,19 @@
+//
+//  BBangChinJa++Bundle.swift
+//  BBangChinJa
+//
+//  Created by Dahlia on 5/13/24.
+//
+
+import Foundation
+
+extension Bundle {
+		var idKey: String {
+				guard let file = self.path(forResource: "Secrets", ofType: "plist") else { return "" }
+				
+				guard let resource = NSDictionary(contentsOfFile: file) else { return "" }
+				guard let key = resource["NaverID"] as? String else { fatalError("아이디가 필요합니다.") }
+				
+				return key
+		}
+}
